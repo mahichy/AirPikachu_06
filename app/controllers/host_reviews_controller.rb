5fe4a208512d288723a5c1483c4d2bb141a31529
mahi <mohiddinoshim@yahoo.com>
2020-04-10 15:16:08 +0600

    after jquery

--- app/controllers/host_reviews_controller.rb
+++ app/controllers/host_reviews_controller.rb
@@ -45,6 +45,6 @@ class HostReviewsController < ApplicationController
 
 	private
 	def host_review_params
-		params.require(:host_review).permit(:comment, :star, :room_id, :reservation_id, :guest_id, :host_id)
+		params.require(:host_review).permit(:comment, :star, :room_id, :reservation_id, :host_id, :guest_id)
 	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,11 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 		@rooms =  @user.rooms
+
+		# Display all the guest reviews to host (if this user is a host)
+		@guest_reviews = Review.where(type: "Guest_review", host_id: @user.id)
+
+				# Display all the host reviews to host (if this user is a guest)
+		@host_reviews = Review.where(type: "Host_review", guest_id: @user.id)
 	end
 end
